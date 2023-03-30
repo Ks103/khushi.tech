f8a52e6c9ee758d04dfbebafa2bd6aa562f8f213
Khushi Shroff <86292101+Ks103@users.noreply.github.com>
2023-03-30 12:33:42 +0530

    #2

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -38,7 +38,7 @@ padding-top: 10rem;
 const Grid = styled.div`
 display: grid;
 grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
-grid-gap: calc(1rem + 2vw);
+grid-gap: calc(2rem + 2vw);
 `
 
 // Framer-motion config
