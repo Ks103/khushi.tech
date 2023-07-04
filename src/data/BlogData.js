22bdeea495d23371e02bf87e1036b48c15fd9c5e
Khushi Shroff <86292101+Ks103@users.noreply.github.com>
2023-07-04 13:45:02 +0530

    Update BlogData.js

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,8 +1,25 @@
 // blog data
 
 export const Blogs = [
-    {
+     {
         id:1,
+        name:"Contributed 10+ repositories in GSSOC'23",
+        tags:["Open source"],
+        
+        // imgSrc:"https://www.istockphoto.com/vector/treasure-hunting-word-concepts-banner-family-time-together-geocaching-presentation-gm1201664891-344706578",
+        
+    },
+     {
+        id:2,
+        name:"Our team MOTION was selected in Maruti Suzuki XCElerate hackathon",
+        tags:["Hackathon"],
+        
+        // imgSrc:"https://www.istockphoto.com/vector/treasure-hunting-word-concepts-banner-family-time-together-geocaching-presentation-gm1201664891-344706578",
+        
+    },
+   
+    {
+        id:3,
         name:"I and my team secured 3rd position in the hunt,it was great event conducted by our GDSC group.",
         tags:["compitition","tressure hunt"],
         
@@ -10,7 +27,7 @@ export const Blogs = [
         link:"https://khushitech.netlify.app/Document/Tressure%20hunt%20winner.pdf"
     },
     {
-        id:2,
+        id:4,
         name:"My team SCIONS have recently participated in Smart India Hackathon, had a great experience.",
         tags:["Smart India Hackathon"],
         
@@ -18,7 +35,7 @@ export const Blogs = [
         link:"https://khushitech.netlify.app/Document/SIH.pdf"
     },
     {
-        id:3,
+        id:5,
         name:"Recently I have joined the web development training, it was a 2 month training in which I gained more knowledge in web domain.",
         tags:["Training","Web development"],
         
@@ -26,7 +43,7 @@ export const Blogs = [
         link:"https://khushitech.netlify.app/Document/Web%20Development%20Training%20-%20Certificate%20of%20Completion.pdf"
     },
     {
-        id:4,
+        id:6,
         name:"Recently I was a  web development intern in TXON, it was a 1 month internship in which I have to perform several tasks.",
         tags:["Internship"],
         
@@ -34,38 +51,12 @@ export const Blogs = [
         link:"https://khushitech.netlify.app/Document/TXN22A174%20(1).pdf"
     },
     {
-        id:5,
+        id:7,
         name:"Recently I contributed to open source in Hacktoberfest'22. We need to merged 4 repos from difeerent repositries, it was a great start to open source. ",
         tags:["Hacktoberfest2022","Open source"],
         
         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
         // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
     },
-    // {
-    //     id:6,
-    //     name:"How to implement Pagination Component in React from scratch",
-    //     tags:["react","css"],
-    //     date:"5 Jan, 2021",
-    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     link:"https://codebucks.hashnode.dev/pagination-in-react"
-
-    // },
-   
-    // {
-    //     id:7,
-    //     name:"What is Redux ? Simply Explained!",
-    //     tags:["react","redux","javascript"],
-    //     date:"16 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
-    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
-    // },
-    // {
-    //     id:8,
-    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
-    //     tags:["react","framer-motion","styled-components"],
-    //     date:"26 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
-    // },
-  
+    
 ] 
